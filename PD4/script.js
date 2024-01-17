e8294fa09c6b643dd9dd0f4de921d06312726160
matasrastauskas <156893786+matasrastauskas@users.noreply.github.com>
2024-01-17 11:43:22 +0200

    Add files via upload

---
+++ PD4/script.js
@@ -0,0 +1,21 @@
+
+let petName;
+let petAge;
+
+
+petName = "Buddy";
+petAge = 3;
+
+
+console.log("Pet Name:", petName);
+console.log("Pet Age:", petAge);
+
+
+petName = "Charlie";
+petAge = 5;
+
+
+const buildingAddress = "123 Main Street";
+
+
+console.log("Building Address:", buildingAddress);
---
+++ PT5/script.js
@@ -0,0 +1,38 @@
+//1
+let numberExample = 42;
+console.log("Number Example:", numberExample);
+//2
+let bigintExample = BigInt(1000000000000000000);
+console.log("Bigint Example:", bigintExample);
+
+//3
+let stringExample = 'Beautiful JavaScript World';
+console.log("String Example:", stringExample);
+
+//4
+let booleanExample = true;
+console.log("Boolean Example:", booleanExample);
+
+//5
+let undefinedExample;
+console.log("Undefined Example:", undefinedExample);
+
+//6
+let nullExample = null;
+console.log("Null Example:", nullExample);
+
+//7
+let objectExample = { key: 'value' };
+console.log("Object Example:", objectExample);
+
+//8
+console.log("Type of 'number':", typeof number);
+console.log("Type of 'otherNumber':", typeof otherNumber);
+console.log("Type of 'textOne':", typeof textOne);
+console.log("Type of 'textTwo':", typeof textTwo);
+console.log("Type of 'textThree':", typeof textThree);
+console.log("Type of 'isTrue':", typeof isTrue);
+console.log("Type of 'isEqual':", typeof isEqual);
+console.log("Type of 'petName':", typeof petName);
+console.log("Type of 'petAge':", typeof petAge);
+console.log("Type of 'human':", typeof human);
