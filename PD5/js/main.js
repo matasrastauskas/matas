3464fd641a1b2ff8d55975fc6459667b234f64b6
matasrastauskas <156893786+matasrastauskas@users.noreply.github.com>
2024-01-25 10:11:59 +0200

    Add files via upload

---
+++ PD5/js/main.js
@@ -0,0 +1,63 @@
+
+let skaicius = 1; 
+console.log(skaicius); 
+
+var bigIntSkaicius = 12345678901111111n; 
+console.log(bigIntSkaicius); 
+
+var stringas = "Stringas"; 
+console.log(stringas); 
+
+var teisingas = true;
+console.log(teisingas); 
+
+var kazkas; 
+console.log(kazkas);
+
+var nullas = null; 
+console.log(nullas); 
+
+var objektas = { vardas: "Matas", amzius: 19, miestas: "Vilnius"}; 
+console.log(objektas); 
+
+
+let number = 12;
+console.log(number);
+console.log(typeof number);
+
+let otherNumber = 45.89;
+console.log(otherNumber);
+console.log(typeof otherNumber);
+
+let textOne = 'Beautiful JavaScript World';
+console.log(textOne);
+console.log(typeof textOne);
+
+let textTwo = 'Beautiful JavaScript World';
+console.log(textTwo);
+console.log(typeof textTwo);
+
+let textThree = 'Beautiful JavaScript World';
+console.log(textThree);
+console.log(typeof textThree);
+
+let isTrue = (true == false);
+console.log(isTrue);
+console.log(typeof isTrue);
+
+let petName = undefined;
+console.log(petName);
+console.log(typeof petName);
+
+let petAge = null;
+console.log(petAge);
+console.log(typeof petAge);
+
+let human = {
+  humanName: undefined,
+  humanAge: null
+};
+console.log(human.humanName);
+console.log(human.humanAge);
+console.log(typeof human.humanName);
+console.log(typeof human.humanAge);
